3679a936efb32e4b055368dc43d90010fdfdcbd4
paikwiki <paikwiki@gmail.com>
2018-05-16 19:10:53 +0900

    Add all monsters on MostersTableSeeder

--- database/seeds/MonstersTableSeeder.php
+++ database/seeds/MonstersTableSeeder.php
@@ -14,16 +14,75 @@ class MonstersTableSeeder extends Seeder
     {
         Monster::create([
             'name' => 'Kraken',
-            'attackPower' => 5,
-            'defensePower' => 5,
+            'attack_power' => 5,
+            'defense_power' => 5,
             'card_id' => 1,
         ]);
         Monster::create([
             'name' => 'Cerberos',
-            'attackPower' => 6,
-            'defensePower' => 9,
+            'attack_power' => 6,
+            'defense_power' => 9,
             'card_id' => 2,
         ]);
-
+        Monster::create([
+            'name' =>'Sphinx',
+            'attack_power' => 10,
+            'defense_power' => 7,
+            'card_id' => 3,
+        ]);
+        Monster::create([
+            'name' =>'Griffin',
+            'attack_power' => 6,
+            'defense_power' => 5,
+            'card_id' => 4,
+        ]);
+        Monster::create([
+            'name' =>'Minotauros',
+            'attack_power' => 10,
+            'defense_power' => 7,
+            'card_id' => 5,
+        ]);
+        Monster::create([
+            'name' =>'Cyclops',
+            'attack_power' => 6,
+            'defense_power' => 9,
+            'card_id' => 6,
+        ]);
+        Monster::create([
+            'name' =>'Siren',
+            'attack_power' => 10,
+            'defense_power' => 7,
+            'card_id' => 7,
+        ]);
+        Monster::create([
+            'name' =>'Basilisk',
+            'attack_power' => 5,
+            'defense_power' => 9,
+            'card_id' => 8,
+        ]);
+        Monster::create([
+            'name' =>'Myrmekes',
+            'attack_power' => 8,
+            'defense_power' => 6,
+            'card_id' => 9,
+        ]);
+        Monster::create([
+            'name' =>'Peryton',
+            'attack_power' => 5,
+            'defense_power' => 9,
+            'card_id' => 10,
+        ]);
+        Monster::create([
+            'name' =>'Satyrs',
+            'attack_power' => 5,
+            'defense_power' => 8,
+            'card_id' => 11,
+        ]);
+        Monster::create([
+            'name' =>'Chimera',
+            'attack_power' => 9,
+            'defense_power' => 6,
+            'card_id' => 12,
+        ]);
     }
 }
