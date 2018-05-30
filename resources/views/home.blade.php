b1a99ca33a068927421cd797f37f95865410a323
paikwiki <paikwiki@gmail.com>
2018-05-30 21:16:12 +0900

    Print image, attack_power and defense_power on Card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,13 @@
                 <div class="card-header">
                     <p>{{ $monster->name }}</p>
                 </div>
+                <div class="card-body">
+                    <img src="{{ $monster->image_url }}" alt="">
+                </div>
+                <div class="card-footer">
+                    <span class="point point-attack">{{ $monster->attack_power }}</span>
+                    <span class="point point-defense">{{  $monster->defense_power }}</span>
+                </div>
             </div>
         </div>
         <div class="card-wrapper">
@@ -19,7 +26,13 @@
                 <div class="card-header">
                     <p>{{ $monster2->name }}</p>
                 </div>
-
+                <div class="card-body">
+                    <img src="{{ $monster2->image_url }}" alt="">
+                </div>
+                <div class="card-footer">
+                    <span class="point point-attack">{{ $monster2->attack_power }}</span>
+                    <span class="point point-defense">{{  $monster2->defense_power }}</span>
+                </div>
             </div>
         </div>
     </div>
